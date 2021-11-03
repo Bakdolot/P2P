b639d6bc0695d5c6a7e024003c287c004f44564a
devvourer <beksultan.toktobekov@mail.ru>
2021-11-03 16:57:40 +0600

    add business logic

--- trading/trade_services.py
+++ trading/trade_services.py
@@ -3,9 +3,26 @@ import requests
 from decimal import Decimal
 
 
-def check_balance(id: str, quantity: Decimal) -> int:
+def check_balance(token: str, quantity: Decimal, sell: int) -> int:
     url = 'some url'
-    response = requests.get(url, headers={'user': id, 'quantity': quantity})
+    response = requests.get(url, headers={'Authorization': 'Basic *******'},
+                            data={'user': token, 'quantity': quantity, 'sell': sell})
+    return response.status_code == 200
+
+
+def debiting_money(token: str, quantity: Decimal, sell: int) -> int:
+    url = 'some url'
+    response = requests.post(url, headers={'Authorization': 'Basic *******'},
+                             data={'user': token, 'quantity': quantity, 'sell': sell},)
+    return response.status_code == 200
+
+
+def make_transaction(owner: str, participant: str, sell: int, buy: int, quantity: int) -> int:
+    url = 'some url'
+    response = requests.post(url, headers={'Authorization': 'Basic *******'},
+                             data={'owner': owner, 'quantity': quantity,
+                                   'sell': sell, 'participant': participant,
+                                   'buy': buy},)
     return response.status_code
 
 
