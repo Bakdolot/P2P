90b8b6448f32ac7e8904d06d29caa63c5545dc20
Bakdolot <genry03gh@gmail.com>
2021-11-03 14:37:42 +0600

    changed model

--- trading/models.py
+++ trading/models.py
@@ -2,10 +2,17 @@ from django.db import models
 
 
 class Trade(models.Model):
-    owner = models.IntegerField()
-    is_active = models.BooleanField(default=True)
-    sell = models.IntegerField()
-    buy = models.IntegerField()
-    quantity = models.DecimalField(max_digits=19, decimal_places=2)
-    create_at = models.DateTimeField(auto_now_add=True)
-    participant = models.IntegerField(blank=True)
+    TYPE_CHOICES = (
+        ('1', 'Крипта'),
+        ('2', 'Карта'),
+        ('3', 'Наличка')
+    )
+
+    owner = models.CharField('Токен продавца', max_length=150)
+    is_active = models.BooleanField('Активность', default=True)
+    sell = models.IntegerField('ID продаваемой крипты', blank=True)
+    buy = models.IntegerField('ID покупаемой крипты', blank=True)
+    quantity = models.DecimalField('Сумма', max_digits=19, decimal_places=2)
+    create_at = models.DateTimeField('Дата создание', auto_now_add=True)
+    participant = models.CharField('Токен покупателья', blank=True, max_length=150)
+    type = models.CharField('Тип', choices=TYPE_CHOICES, max_length=30)
