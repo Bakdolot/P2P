15ba5c4dd4022d1d603ba8584542c361102361e9
devvourer <beksultan.toktobekov@mail.ru>
2021-11-09 11:08:15 +0600

    fix

--- trading/trade_services.py
+++ trading/trade_services.py
@@ -11,18 +11,19 @@ def checking_and_debiting_balance(login: str, quantity: Decimal, currency: int) 
     """ Проверка баланса, если на балансе достаточно средств - они
         списываются со счета, в противном случае сделка не может быть создана
     """
+    currency = EtCurrency.objects.get(id=currency)
+
     try:
-        balance = EtBalance.objects.get(login=login, currency='DOGE')
+        balance = EtBalance.objects.get(login=login, currency=currency.alias)
         if Decimal(balance.balance) >= Decimal(quantity):
             balance.balance = str(Decimal(balance.balance) - Decimal(quantity))
             balance.save(update_fields=['balance'])
             return True
-        else:
-            print('NO')
-
     except Exception as e:
         print(e)
         return False
+
+    return False
 
 
 def make_transaction(owner: str, participant: str, sell: int, buy: int, quantity: int) -> int:
