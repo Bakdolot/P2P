17955ebd8e82f5cebb38323edb93edf3bbba2c6c
Bakdolot <genry03gh@gmail.com>
2021-11-11 17:52:58 +0600

    some text

--- trading/views.py
+++ trading/views.py
@@ -76,7 +76,7 @@ class TradeUpdateView(generics.RetrieveUpdateDestroyAPIView):
 
 
 class AcceptCardReceivedPaymentTradeView(generics.GenericAPIView):
-    queryset = Trade.objects.all()
+    queryset = Trade.objects.filter(type='card', status='process', participant_sent=False)
     permission_classes = [IsOwner]
     
     def put(self, request, *args, **kwargs):
@@ -146,8 +146,9 @@ class AcceptTradeView(generics.GenericAPIView):  # Наличка
 
 
 class AcceptCardSentPaymentTradeView(generics.RetrieveUpdateAPIView):  # Карта
-    queryset = Trade.objects.filter(is_active=True, type='card')
+    queryset = Trade.objects.filter(is_active=True, type='card', participant_sent=False)
     serializer_class = AcceptCardPaymentTradeSerializer
+    permission_classes = [IsParticipant]
 
     def put(self, request, *args, **kwargs):
         if super().put(request, *args, **kwargs):
