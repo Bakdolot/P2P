ae8522002d215e9323572cb501f173d3d6857ad8
Bakdolot <genry03gh@gmail.com>
2021-11-23 18:31:04 +0600

    added pay24 xml files

--- Pay24/models.py
+++ Pay24/models.py
@@ -15,8 +15,13 @@ class Category(models.Model):
 class Service(models.Model):
     name = models.CharField(max_length=120)
     api_id = models.IntegerField()
-    logo = models.ImageField(upload_to='Pay24/'),
+    logo = models.ImageField(upload_to='Pay24/')
     order_id = models.SmallIntegerField()
+    category = models.SmallIntegerField()
+    commission = models.CharField(max_length=12)
+    min_sum  = models.CharField(max_length=12)
+    max_sum  = models.CharField(max_length=12)
+    support_phone = models.CharField(max_length=24)
 
     class Meta:
         db_table = 'et_pay24_services'
@@ -28,12 +33,13 @@ class Pay24Operation(models.Model):
     login = models.CharField(max_length=64)
     operation_type = models.SmallIntegerField()
     sum = models.CharField(max_length=32)
+    sum_with_commission = models.CharField(max_length=32)
     currency = models.CharField(max_length=12)
     guid = models.CharField(max_length=120, blank=True, null=True)
     ip_address = models.CharField(max_length=32)
     date_creation = models.CharField(max_length=32, default=datetime.now().timestamp(), blank=True, null=True)
     date_update = models.CharField(max_length=32, blank=True, null=True)
-    commission = models.CharField(max_length=12, blank=True, null=True)
+    reference = models.IntegerField()
 
     def save(self, *args, **kwargs):
         self.date_update = datetime.now().timestamp()
