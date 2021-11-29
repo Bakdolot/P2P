1c6dcbe692ea48609fe51302b9c44fe0a3cbbb4d
Bakdolot <genry03gh@gmail.com>
2021-11-29 14:30:17 +0600

    converted unixtime format to datetime format

--- trading/utils.py
+++ trading/utils.py
@@ -1,6 +1,7 @@
 from django.db.models.signals import pre_save
 from .models import Trade
 from internal_transfer.services import get_sum_with_commission
+from datetime import datetime
 
 
 def my_callback(sender, instance, *args, **kwargs):
@@ -8,3 +9,6 @@ def my_callback(sender, instance, *args, **kwargs):
 
 pre_save.connect(my_callback, sender=Trade)
 
+
+def convert_unixtime_to_datetime(unixtime: str) -> str:
+    return datetime.utcfromtimestamp(int(unixtime)).strftime('%Y-%m-%d %H:%M:%S')
